4595ce2d08ee5ec98f5df8d179f3afb172c30e4e
amir-p <amir-pasalic@outlook.com>
2019-12-01 14:38:14 +0100

    Test queries in progress and general refactoring

--- CLIScripts/initialize.sh
+++ CLIScripts/initialize.sh
@@ -4,11 +4,3 @@
 docker-compose -f "../docker-compose-postgres.yml" up -d --build
 sleep 1s
 docker exec -it postgres-db bash ./DatabaseScripts/CreateDatabase.sh
-
-#Copy CreateDatabase.sql and execute it in the postgres docker container
-# Run query on sql schema and show result
-# Run query on JSONB schema and show result
-# Apply indexes 
-# Run query on sql schema and show result
-# Run query on JSONB schema and show result
-# Output the result in output summary file
--- DatabaseScripts/Queries/CarReservationsTestQueries.sql
+++ DatabaseScripts/Queries/CarReservationsTestQueries.sql
@@ -1,11 +1,106 @@
 \connect CarReservationsDb;
 
-SELECT cr.id, cars.id, cars.brand, cars.model, cars.companyname
-	FROM car_reservations as cr
-	INNER JOIN cars on cr.carid = cars.id
-	WHERE cars.model = 'X5';
+------------------------------------------------
+-- Example 1.
+-- Select all from Cars table where the model is 'X5'.
+
+-- Standard SQL
+
+SELECT *
+FROM cars
+WHERE model = 'X5';
+ANALYZE;
+
+
+-- JSONB
+
+SELECT *
+FROM jsonb_cars
+WHERE (data ->> 'model')::VARCHAR AS model = 'X5';
+ANALYZE;
+
+
+------------------------------------------------
+-- Example 2.
+-- Select all from Cars table where the model is 'X1'
+-- or model is X3 or model is X5.
+
+-- Standard SQL
+
+SELECT *
+FROM cars
+WHERE model = 'X1' OR model = 'X3' OR model = 'X5';
+ANALYZE;
+
+
+-- JSONB
+
+SELECT *
+FROM jsonb_cars
+WHERE 
+	(data ->> 'model')::VARCHAR AS model = 'X1' OR
+	(data ->> 'model')::VARCHAR AS model = 'X3' OR
+	(data ->> 'model')::VARCHAR AS model = 'X5';
+ANALYZE;
+
+
+------------------------------------------------
+-- Example 3.
+-- Select all Cars where a Reservation exists
+
+-- Standard SQL
+
+SELECT *
+FROM cars 
+INNER JOIN car_reservations AS cr on car.id = cr.car_id
+
+
+-- JSONB
+
+SELECT * -- id, brand, model, company_name
+FROM (
+  SELECT 
+	id,
+	(data ->> 'car_id')::INTEGER AS car_id
+  FROM jsonb_car_reservations
+) tmp_jsonb_car_reservations
+JOIN jsonb_cars AS cr
+  ON (cr.id = tmp_jsonb_car_reservations.car_id);
+  
+ ANALYZE;
+
+
+------------------------------------------------
+------------------------------------------------
+------------------------------------------------
+
+
+-- Example ....
+
+-- Standard SQL
 
 SELECT COUNT(cr.id)
 	FROM car_reservations as cr
-	INNER JOIN cars on cr.carid = cars.id
+	INNER JOIN cars on cr.car_id = cars.id
 	WHERE cars.model = 'X5';
+
+ANALYZE;
+
+-- JSONB
+
+SELECT COUNT(*) -- id, brand, model, company_name
+FROM (
+  SELECT 
+	id,
+	(data ->> 'brand')::VARCHAR AS brand,
+	(data ->> 'model')::VARCHAR AS model,
+	(data ->> 'company_name')::VARCHAR AS company_name
+  FROM jsonb_cars
+  WHERE
+    (data ->> 'model')::VARCHAR = 'X5'
+) tmp_jsonb_cars
+JOIN jsonb_car_reservations AS cr
+  ON (cr.id = tmp_jsonb_cars.id); --here should be cr.car_id = tmp_jsonb_cars.id but the cr.car_id is 
+  -- in JSONB so this is not relevan query
+  
+ ANALYZE;
