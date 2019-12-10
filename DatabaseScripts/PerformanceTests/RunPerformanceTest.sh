2f0c5927bed48ce5df2201e677c2ded196ae070e
amir-p <amir-pasalic@outlook.com>
2019-12-10 20:52:38 +0100

    develop: Refactoring RunPerformanceTest.sh

--- DatabaseScripts/PerformanceTests/RunPerformanceTest.sh
+++ DatabaseScripts/PerformanceTests/RunPerformanceTest.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+logFile="/DatabaseScripts/PerformanceTestResults/PerformanceTestLog.txt"
+truncate -s 0 logFile 
+
+function execute_query {
+    cat $1 >> $logFile
+    echo "" >> $logFile
+    psql -d "CarReservationsDb" -f $1 >> $logFile
+    echo "" >> $logFile
+}
+
 cd DatabaseScripts/PerformanceTests/Queries
-echo 'Run first query...'
-
-cat Query1.sql >> PerformanceTestLog.txt
-echo "" >> PerformanceTestLog.txt
-psql -d "CarReservationsDb" -f Query1.sql >> PerformanceTestLog.txt
-echo "" >> PerformanceTestLog.txt
-
-cat Query1JSONB.sql >> PerformanceTestLog.txt
-echo "" >> PerformanceTestLog.txt
-psql -d "CarReservationsDb" -f Query1JSONB.sql >> PerformanceTestLog.txt
-echo "" >> PerformanceTestLog.txt
-
+echo 'Running queries...'
+echo "" >> $logFile
+execute_query Query1.sql
+execute_query Query1JSONB.sql
+echo 'Running quries finished!'
 echo 'Full Log of Performance test:'
-cat PerformanceTestLog.txt;
+cat $logFile;
