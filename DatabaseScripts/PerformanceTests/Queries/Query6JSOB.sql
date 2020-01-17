7f65a6a1a4e143c2c2a048b37fc3151972e0b7f7
amir-p <amir-pasalic@outlook.com>
2020-01-17 20:07:52 +0100

    develop: Add more queries

--- DatabaseScripts/PerformanceTests/Queries/Query6JSOB.sql
+++ DatabaseScripts/PerformanceTests/Queries/Query6JSOB.sql
@@ -8,5 +8,34 @@
 -- the customer has reserved a car with brand VW and model Touareg.
 -- JSONB
 
---EXPLAIN ANALYSE
--- TBD
+EXPLAIN ANALYSE
+SELECT 
+  (tmp_jsonb_car_reservations.id)::INTEGER AS car_reservations_id,
+  (ca.data ->> 'brand')::VARCHAR AS car_brand, 
+  (ca.data ->> 'model')::VARCHAR AS car_model, 
+  (cus.data ->> 'first_name'):: VARCHAR AS customer_first_name, 
+  (cus.data ->> 'last_name'):: VARCHAR AS customer_last_name,
+  (tmp_jsonb_car_reservations.start_date):: DATE AS reservation_start_date,
+  (tmp_jsonb_car_reservations.end_date):: DATE AS reservation_end_date
+FROM (
+  SELECT 
+	  id,
+	  (data ->> 'car_id')::INTEGER AS car_id,
+  	(data ->> 'customer_id')::INTEGER AS customer_id,
+	  (data ->> 'start_date')::DATE AS start_date,
+	  (data ->> 'end_date')::DATE AS end_date
+  FROM jsonb_car_reservations
+) tmp_jsonb_car_reservations
+INNER JOIN jsonb_cars AS ca
+  ON (ca.id = tmp_jsonb_car_reservations.car_id)
+INNER JOIN jsonb_customers AS cus
+  ON (cus.id = tmp_jsonb_car_reservations.customer_id)
+WHERE
+    (tmp_jsonb_car_reservations.start_date BETWEEN 
+            (SELECT 'tomorrow'::DATE tomorrow) 
+                AND	
+            (SELECT (SELECT NOW() + INTERVAL '7 DAY')::DATE) 
+    ) AND
+    ca.data ->> 'brand' = 'VW' AND 
+    ca.data ->> 'model' = 'Touareg' AND
+    cus.data ->> 'first_name' = 'Max';
--- DatabaseScripts/PerformanceTests/Queries/Query7.sql
+++ DatabaseScripts/PerformanceTests/Queries/Query7.sql
@@ -2,8 +2,13 @@
 ------------------------------------------------------------
 
 -- Example 7.
--- TBD
+-- Select all car_reservatins with customer information 
+-- which has specific customer id. For example the customer.id = 5
 -- Standard SQL
 
-EXPLAIN ANALYSE  
--- sql here
+EXPLAIN ANALYSE
+SELECT cr.id AS carReservations_Id, cus.first_name, cus.last_name, 
+	   cr.start_date AS carReservations_start_date, cr.end_date AS carReservations_end_date
+FROM car_reservations AS cr
+INNER JOIN customers AS cus ON cr.customer_id = cus.id
+WHERE cus.id = 5;
