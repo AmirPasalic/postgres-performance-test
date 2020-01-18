427056913152211b7ba788723a06fec9d1b990fa
amir-p <amir-pasalic@outlook.com>
2020-01-18 22:36:47 +0100

    develop: Refactoring

--- DatabaseScripts/TemporaryQueries/CarReservationsTestQueries.sql
+++ DatabaseScripts/TemporaryQueries/CarReservationsTestQueries.sql
@@ -1,43 +1,6 @@
-\connect CarReservationsDb;
+-- Returns table
 
-------------------------------------------------
-------------------------------------------------
-------------------------------------------------
-
-
--- Example ....
-
--- Standard SQL
-
-SELECT COUNT(cr.id)
-	FROM car_reservations as cr
-	INNER JOIN cars on cr.car_id = cars.id
-	WHERE cars.model = 'X5';
-
-ANALYZE;
-
--- JSONB
-
-SELECT COUNT(*) -- id, brand, model, company_name
-FROM (
-  SELECT 
-	id,
-	(data ->> 'brand')::VARCHAR AS brand,
-	(data ->> 'model')::VARCHAR AS model,
-	(data ->> 'company_name')::VARCHAR AS company_name
-  FROM jsonb_cars
-  WHERE
-    (data ->> 'model')::VARCHAR = 'X5'
-) tmp_jsonb_cars
-JOIN jsonb_car_reservations AS cr
-  ON (cr.id = tmp_jsonb_cars.id); --here should be cr.car_id = tmp_jsonb_cars.id but the cr.car_id is 
-  -- in JSONB so this is not relevan query
-  
- ANALYZE;
-
- ------
-
- CREATE OR REPLACE FUNCTION queryAmir() 
+CREATE OR REPLACE FUNCTION testFunction() 
 	RETURNS 
 	TABLE(id BIGINT, car_brand VARCHAR, car_model VARCHAR) 
 	AS $func$
@@ -59,39 +22,16 @@ BEGIN
 END;
 $func$ LANGUAGE plpgsql;
 
-SELECT * FROM queryAmir();
+-- run it
 
--- DROP FUNCTION queryAmir()
+SELECT * FROM testFunction();
 
-----------------------------------------------------------
+-- DROP FUNCTION testFunction()
 
-CREATE OR REPLACE FUNCTION queryAmir() 
-	RETURNS 
-	TABLE(id BIGINT, car_brand VARCHAR, car_model VARCHAR) 
-	AS $func$
-DECLARE
-	sevenDaysInTheFuture DATE := (SELECT (SELECT NOW() + INTERVAL '7 DAY')::DATE);
-	tomorrow DATE := (SELECT 'tomorrow'::DATE tomorrow);
-BEGIN 
-	RETURN QUERY
-	-- EXPLAIN ANALYSE
-    SELECT cr.id, ca.brand, ca.model 
-	FROM car_reservations AS cr
-    INNER JOIN cars AS ca ON cr.car_id = ca.id
-    INNER JOIN customers AS cus ON cr.customer_id = cus.id
-    WHERE 
-        (cr.start_date BETWEEN tomorrow AND sevenDaysInTheFuture) OR
-        (ca.brand = 'VW' OR 
-        ca.model = 'Touareg' OR
-        cus.first_name = 'Max');
-END;
-$func$ LANGUAGE plpgsql;
+------------------------------------------------
+------------------------------------------------
 
-SELECT * FROM queryAmir();
-
-------
-
--- To return Explain analyze
+-- To return Explain Analyze
 
 CREATE OR REPLACE FUNCTION queryAmir() 
 	RETURNS 
@@ -115,5 +55,3 @@ BEGIN
 		RETURN analyzeResult;
 END;
 $func$ LANGUAGE plpgsql;
-
-
